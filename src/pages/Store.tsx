66337a7614790367afa353ed184b3bae1025f437
Eng Omar <engomaralryan@gmail.com>
2024-08-26 22:03:16 +0300

    add the store and some components ... .

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,5 +1,20 @@
+import { Col, Row } from "react-bootstrap";
+import storeData from "../data/items.json";
+import StoreItems from "../components/StoreItems";
+
 function Store() {
-  return <div>Store</div>;
+  return (
+    <>
+      <h1>Store</h1>
+      <Row xs={1} md={2} lg={3} className="g-3">
+        {storeData.map((item) => (
+          <Col key={item.id}>
+            <StoreItems {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 }
 
 export default Store;
