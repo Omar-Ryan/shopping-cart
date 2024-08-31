1531f1ade5c6571a8fa67cd7a5ac03f65fbbf6f8
Eng Omar <engomaralryan@gmail.com>
2024-08-31 08:00:19 +0300

    complete and some the chaninges ...

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,63 @@
+import { Card, CardHeader, Stack } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+
 function About() {
-  return <div>About</div>;
+  return (
+    <Stack className="d-flex justify-content-center align-items-center">
+      <Card className="p-3 m-auto mt-5">
+        <CardHeader
+          style={{
+            fontWeight: "bolder",
+            fontSize: "18px",
+            padding: "14px 0px",
+          }}
+        >
+          Registration
+        </CardHeader>
+        <Form>
+          <Row className="mb-3">
+            <Form.Group as={Col} controlId="formGridEmail" className="mt-2">
+              <Form.Label className="fw-bold">Email</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridPassword" className="mt-2">
+              <Form.Label className="fw-bold">Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+          </Row>
+
+          <Row className="mb-3">
+            <Form.Group as={Col} className="mb-3" controlId="formGridAddress1">
+              <Form.Label className="fw-bold">Address</Form.Label>
+              <Form.Control placeholder="1234 Main St" />
+            </Form.Group>
+
+            <Form.Group as={Col} className="mb-3" controlId="formGridAddress2">
+              <Form.Label className="fw-bold">Mobile number</Form.Label>
+              <Form.Control type="number" placeholder="+20 01011111111" />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridZip">
+              <Form.Label className="fw-bold">Zip</Form.Label>
+              <Form.Control />
+            </Form.Group>
+          </Row>
+
+          <Form.Group className="mb-3" id="formGridCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Card>
+    </Stack>
+  );
 }
 
 export default About;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,24 @@
+import { Stack } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="home">O.R</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="home">Home</Nav.Link>
+            <Nav.Link href="store">Store</Nav.Link>
+            <Nav.Link href="about">About</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Stack className="mt-3">The Home</Stack>
+    </div>
+  );
 }
 
 export default Home;
